709e1321c238dc49c63de0d5b5f770fe28959ccd
LiamNunes <liam.nunes@hotmail.com>
2024-08-05 22:33:05 +0000

    [ADD] fields to motorcycle registry model

--- motorcycle_registry/models/motorcycle_registry.py
+++ motorcycle_registry/models/motorcycle_registry.py
@@ -1,5 +1,15 @@
-from odoo import models
+from odoo import fields, models
 
 class MotorcycleRegistry(models.Model):
     _name = 'motorcycle.registry'
     _description = 'Motorcycle Registry'
+    _record_name = "registry_number"
+
+    registry_number = fields.Char(string='Registry Number', required=True)
+    certificate_title = fields.Binary(string='Certificate Title')
+    current_mileage = fields.Float(string='Current Mileage')
+    date_registry = fields.Date(string='Date of Registry')
+    first_name = fields.Char(string='First Name', required=True)
+    last_name = fields.Char(string='Last Name', required=True)
+    license_plate = fields.Char(string='License Plate')
+    vin = fields.Char(string='VIN', required=True)
