a26c37dcf97d0b81e718da50eabeac2d78e0a4d4
LiamNunes <liam.nunes@hotmail.com>
2024-08-05 01:45:30 +0000

    [ADD] New Module

---
+++ motorcycle_registry/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Motorcycle Registry',
+    'website':'https://github.com/LiamNunes/KawiilMotors',
+    'version': '0.0.1',
+    'category': 'Kawiil/Custom Modules',
+    'summary': 'Manage Registration of Motorcyles',
+    'description': """
+        Motorcycle Registry
+        ===================
+        This Module is used to keep track of the Motorcycle Registration and Ownership 
+        of each motorcyle of the brand
+    """,
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': True,
+}
